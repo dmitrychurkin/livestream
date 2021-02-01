fc2f13bf66b6e88fde8dabd430257390ce2ae7bf
Dmitry Churkin <dchurkin@cuddlynest.com>
2021-02-01 15:44:54 +0200

    migrated to Next.js, continue implementation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const withPlugins = require("next-compose-plugins");
 const withSvgr = require("next-svgr");
 
-module.exports = withPlugins([
-  withSvgr
-]);
+module.exports = withPlugins([withSvgr]);
